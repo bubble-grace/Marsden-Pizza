8312a5c6a0346c4f16e898b93190aafaba13b027
bubble-grace <grace.cinque@marsden.school.nz>
2021-07-07 10:05:45 +1200

    Sprint 2 has working lists + validation

--- main_pizza.py
+++ main_pizza.py
@@ -1,42 +1,54 @@
-def print_reciept(L, total_cost):
+
+def print_reciept(C):
     print("----" * 10)
-    for i in range(0, len(L)):
-        if L[i][2] > 0:
-            output = "You have ordered {} {} pizza/s at ${} each".format(L[i][2], L[i][0],  L[i][1])
+    for i in range(0, len(C)):
+        if C[i][2] > 0:
+            output = "You have ordered {} {} pizza/s at ${} each".format(C[i][2], C[i][0],  C[i][1])
             print(output)
-    amount = total_calculator(L, total_cost)
+    amount = total_calculator(C)
     output2 = "This total is ${} ". format(amount)
     print(output2)
     print("----" * 10)
 
 def print_with_indexes(L):
     for i in range(0, len(L)):
-        output = "{}: {}: {} : {}".format(i, L[i][0], L[i][1], L[i][2])
+        output = "{:3}: {:15}: {:6} ".format(i, L[i][0], L[i][1])
         print(output)
 
-def total_calculator(L, total_cost):
-    total = 0
+def print_with_indexes_2(L):
     for i in range(0, len(L)):
-        sub = L[i][2] * L[i][1]
+        output = "{:3}: {:15}: {:6} : {:3} ".format(i, L[i][0], L[i][1], L[i][2])
+        print(output)
+
+def total_calculator(C):
+    total = 0
+    for i in range(0, len(C)):
+        sub = C[i][2] * C[i][1]
         total += sub
     return total
 
 
-def add_pizza(L, total_cost):
+def add_pizza(L, C, max_per_pizza):
     print_with_indexes(L)
-    my_index = get_user_input(0, 2, "Please select what index number, would you like to add pizza to?   ")
-    new_amount = get_user_input(1, 5, "How many of the pizza do you want to add?     ")
-    L[my_index][2] += new_amount
-    print_reciept(L, total_cost)
+    my_index = get_user_input(0, len(L)-1, "Please select what index number, would you like to add pizza to?   ")
+    new_amount = get_user_input(1, max_per_pizza, "How many of the {}/s do you want to add?     " .format(L[my_index][0]))
+    temp = [L[my_index][0], L[my_index][1], new_amount]
+    C.append(temp)
+    print_reciept(C)
 
 
-def subtract_pizza(L, total_cost):
-    print_with_indexes(L)
-    my_index = get_user_input(0, 2, "Please select what index number, would you like to subtract pizza from?   ")
-    new_amount = get_user_input(1, 5, "How many of the pizza do you want to subtract?     ")
-    L[my_index][2] -= new_amount
-    print_reciept(L, total_cost)
 
+def subtract_pizza(C):
+    print_with_indexes_2(C)
+    my_index = get_user_input(0, len(C)-1, "Please select what index number, would you like to subtract pizza from?   ")
+    sub_amount = get_user_input(0, C[my_index][2], "How many of the pizza do you want to subtract?     ")
+    new_amount = C[my_index][2] - sub_amount
+    C[my_index][2] = new_amount
+    print_reciept(C)
+
+# takes two numbers and one string
+# higher and lowers numbers are included in validation
+# output is the message to the user
 def get_user_input (lower, higher, output):
     getting_input = True
     while getting_input == True:
@@ -54,34 +66,41 @@ def get_user_input (lower, higher, output):
 
 def main_loop():
     run = True
-    total_cost = 0
-    my_L = [
-        ["Margarita", 18.5, 0],
-        ["Piccolo", 18.5, 0],
-        ["La Prima Donna", 25.5, 0],
+    max_per_pizza = 5
+    pizza_list = [
+        ["Margarita", 18.5],
+        ["Piccolo", 18.5],
+        ["La Prima Donna", 25.5]
     ]
+    my_menu = [
+        ["q", "quit"],
+        ["m", "the see the menu"],
+        ["a", "to add pizzas to the order"],
+        ["s", "to subtract a pizza/s"],
+        ["r", "to see where the order is at"],
+    ]
+    customer_list =[]
     while run == True:
-        my_menu = """
-        Press 
-        'q' to quit
-        'r' to review the see the menu
-        'a' to add pizzas to the order
-        's' to subtract a pizza/s
+        for i in range(0, len(my_menu)):
+            output = "{} : {}".format(my_menu[i][0], my_menu[i][1])
+            print(output)
 
-        """
-        print(my_menu),
-        user_choice = input("please enter your option here:    ")
+
+        user_choice = input("please enter your option here: ->  ")
         if user_choice == "q":
             print("Loop has stopped, Thank you")
             run = False
-        elif user_choice == "r":
-            print_with_indexes(my_L)
+        elif user_choice == "m":
+            print_with_indexes(pizza_list)
         elif user_choice == "a":
-            add_pizza(my_L, total_cost)
+            add_pizza(pizza_list, customer_list, max_per_pizza)
         elif user_choice == "s":
-            subtract_pizza(my_L, total_cost)
+            subtract_pizza(customer_list)
+        elif user_choice == "r":
+            print_reciept(customer_list)
         else:
             print("sorry your answer was not valid")
+            print("----" * 10)
 
 
 main_loop()
