339c8dd2a788cec3e28958154463772bb4f37d3b
bubble-grace <grace.cinque@marsden.school.nz>
2021-07-26 09:55:17 +1200

    Sprint 4

--- main_pizza.py
+++ main_pizza.py
@@ -1,6 +1,6 @@
 import re
 
-def delivery_option(D):
+def delivery_option(D, total_amount):
     output = """
     1: Delivery, this comes with an extra cost of $3 
     2: Pick up, you come in store to pick up at no extra cost 
@@ -16,7 +16,9 @@ def delivery_option(D):
         print("Your pizza/s are now under the name {}".format(name))
         print("The phone number the pizza/s is associated with will be {}".format(number))
         temp = [name, number, address]
-        D.append (temp)
+        D.append(temp)
+        total_amount += 3
+        return total_amount
     elif choice == 2:
         print("You have chosen pickup ")
         name = check_varible(2, 25, "Please enter your name:     ")
@@ -25,17 +27,23 @@ def delivery_option(D):
         print("The phone number the pizza/s is associated with will be {}".format(number))
         temp = [name, number]
         D.append(temp)
+    return D
 
 def check_phone_number(output):
     getting_input = True
     while getting_input == True:
-        try:
-            user_input = int(input(output))
-        except ValueError:
-            print("You have not entered an integer")
+        user_input = (input(output))
+        x = re.search("(^\([0][3]\))(\d{7}$)|(^\([0][4]\))(\d{7}$)|(^\([0][6]\))(\d{7}$)|(^\([0][7]\))(\d{7}$)|(^\([0][9]\))(\d{7}$)|"
+                      "(^\([0][2][1]\))(\d{6,8}$)|(^\([0][2][2]\))(\d{6,8}$)|(^\([0][2][7]\))(\d{6,8}$)|(^\([0][2][9]\))(\d{6,8}$)|"
+                      "([0][8][0][0])([\s])(\d{5,8}$)", user_input)
+        if x:
+            getting_input = False
+        else:
+            print("Sorry your phone number was not at match")
             continue
-        getting_input = False
     return user_input
+
+
 
 def check_varible (min, max, output):
     get_name = True
@@ -53,14 +61,14 @@ def check_varible (min, max, output):
 
     return name
 
-def print_reciept(C):
+def print_reciept(C, total_amount):
     print("----" * 10)
     for i in range(0, len(C)):
         if C[i][2] > 0:
             output = "You have ordered {} {} pizza/s at ${} each".format(C[i][2], C[i][0],  C[i][1])
             print(output)
-    amount = total_calculator(C)
-    output2 = "This total is ${} ". format(amount)
+    total_amount += total_calculator(C)
+    output2 = "This total is ${} ". format(total_amount)
     print(output2)
     print("----" * 10)
 
@@ -82,23 +90,23 @@ def total_calculator(C):
     return total
 
 
-def add_pizza(L, C, max_per_pizza):
+def add_pizza(L, C, max_per_pizza, total_amount):
     print_with_indexes(L)
     my_index = get_user_input(0, len(L)-1, "Please select what index number, would you like to add pizza to?   ")
     new_amount = get_user_input(1, max_per_pizza, "How many of the {}/s do you want to add?     " .format(L[my_index][0]))
     temp = [L[my_index][0], L[my_index][1], new_amount]
     C.append(temp)
-    print_reciept(C)
+    print_reciept(C, total_amount)
 
 
 
-def subtract_pizza(C):
+def subtract_pizza(C, total_amount):
     print_with_indexes_2(C)
     my_index = get_user_input(0, len(C)-1, "Please select what index number, would you like to subtract pizza from?   ")
     sub_amount = get_user_input(0, C[my_index][2], "How many of the pizza do you want to subtract?     ")
     new_amount = C[my_index][2] - sub_amount
     C[my_index][2] = new_amount
-    print_reciept(C)
+    print_reciept(C, total_amount)
 
 # takes two numbers and one string
 # higher and lowers numbers are included in validation
@@ -133,9 +141,11 @@ def main_loop():
         ["s", "to subtract a pizza/s"],
         ["r", "to see where the order is at"],
         ["d", "to chose the method of receiving your pizzas"],
+        ["c", "to confirm the details of the order"],
     ]
     customer_list =[]
     customer_details =[]
+    total_amount = 0
     while run == True:
         for i in range(0, len(my_menu)):
             output = "{} : {}".format(my_menu[i][0], my_menu[i][1])
@@ -149,13 +159,13 @@ def main_loop():
         elif user_choice == "m":
             print_with_indexes(pizza_list)
         elif user_choice == "a":
-            add_pizza(pizza_list, customer_list, max_per_pizza)
+            add_pizza(pizza_list, customer_list, max_per_pizza, total_amount)
         elif user_choice == "s":
-            subtract_pizza(customer_list)
+            subtract_pizza(customer_list, total_amount)
         elif user_choice == "r":
-            print_reciept(customer_list)
+            print_reciept(customer_list, total_amount)
         elif user_choice == "d":
-            delivery_option(customer_details)
+            total_amount = delivery_option(customer_details, total_amount)
         else:
             print("sorry your answer was not valid")
             print("----" * 10)
